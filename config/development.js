7b19888d124b5cb9f528390ae250f92672b162a5
Roma Bronstein <romabronstein@Romas-MacBook-Pro.local>
2017-11-19 09:04:23 +0200

    initial commit

---
+++ config/development.js
@@ -0,0 +1,57 @@
+
+
+
+const f = (data, cb) => {
+    // do stuff with data
+    if(someError)
+        return cb(someError)
+    return cb(null, response)
+}
+
+
+f(data,(err,data) =>{
+    (er,data) => {
+        // ...
+    }
+})
+
+
+const x = () => new Promise((resolve, reject) => {
+    // async work
+    if(someError)
+        return reject(someError)
+    resolve(data)
+})
+
+
+// const y = ...
+
+return x
+.then(y)
+.catch(err)
+.then(z)
+.catch(y)
+.catch(err=>{/*log & return err resp*/})
+
+
+
+class values {
+    constructor(props) {
+        super(props)
+        this.value = null
+    }
+
+    getValue() {
+        if(readdirSync.get(value)
+            return Promise.resolve(this.value)
+        
+        retireveValueLogic()
+        .then(data => this.value = data)
+        .then(() => this.value)
+    }
+    
+
+}
+
+values.getValue().then(data => {/*...*/})
+
---
+++ config/staging.js
@@ -0,0 +1,3 @@
+export default {
+    someParam: 'stage param'
+}
